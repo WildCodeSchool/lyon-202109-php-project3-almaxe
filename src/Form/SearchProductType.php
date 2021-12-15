480e6997a36b63ee509d4724763bbdb53a182d3b
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-12-15 16:51:49 +0100

    init SearchProductType file

---
+++ src/Form/SearchProductType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('keyWords', SearchType::class, [
+                'label' => false,
+            ])
+            ->add('Search', SubmitType::class, [
+                'label' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
