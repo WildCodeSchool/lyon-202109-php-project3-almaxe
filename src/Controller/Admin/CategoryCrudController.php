6c5277b761fbad0af2e1ae6d0e48e350e6911499
Valentin <valentingaudin@gmail.com>
2022-01-06 09:09:31 +0100

    Remove dead code

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -11,15 +11,4 @@ class CategoryCrudController extends AbstractCrudController
     {
         return Category::class;
     }
-
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }
